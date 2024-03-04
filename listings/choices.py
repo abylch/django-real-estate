6ea031cafdb3c3af72c14b365e145b348558306a
abylch <abylch@hotmail.com>
2024-03-04 12:00:29 +0200

    search functionality

---
+++ listings/choices.py
@@ -0,0 +1,37 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'$100,000',
+  '200000':'$200,000',
+  '300000':'$300,000',
+  '400000':'$400,000',
+  '500000':'$500,000',
+  '600000':'$600,000',
+  '700000':'$700,000',
+  '800000':'$800,000',
+  '900000':'$900,000',
+  '1000000':'$1M',
+  '1000001':'$1M+',
+}
+
+state_choices = {
+        "Jerusalem": "Jerusalem District",
+        "North": "Northern District",
+        "Haifa": "Haifa District",
+        "Central": "Central District",
+        "Tel-Aviv Area": "Tel Aviv District",
+        "South": "Southern District",
+        "Judea and Samaria": "Judea and Samaria Area",
+        "INT": "International",
+}
--- listings/views.py
+++ listings/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 def index(request):
   listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -28,4 +28,58 @@ def listing(request, listing_id):
   return render(request, 'listings/listing.html', context)
 
 def search(request):
-  return render(request, 'listings/search.html')
+  queryset_list = Listing.objects.order_by('-list_date')
+
+  # Keywords
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(description__icontains=keywords)
+
+  # City
+  if 'city' in request.GET:
+    city = request.GET['city']
+    if city:
+      queryset_list = queryset_list.filter(city__iexact=city)
+
+  # State
+  if 'state' in request.GET:
+    state = request.GET['state']
+    if state:
+      if state == 'INT':    
+          # Include all regions except the 7 specified above
+          excluded_states = ["Jerusalem", "North", "Haifa", "Central", "Tel-Aviv Area", "South", "Judea and Samaria"]
+          queryset_list = queryset_list.exclude(state__in=excluded_states)
+      else:
+          # Filter by the selected state
+          queryset_list = queryset_list.filter(state__iexact=state)
+
+  # Bedrooms
+  if 'bedrooms' in request.GET:
+    bedrooms = request.GET['bedrooms']
+    if bedrooms:
+      queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+  # Price
+  if 'price' in request.GET:
+      price = request.GET['price']
+      if price:
+          if price == '1000000':
+              # Price up to $1M
+              queryset_list = queryset_list.filter(price__lte=1000000)
+          elif price == '1000001':
+              # Price bigger than $1M
+              queryset_list = queryset_list.filter(price__gt=1000000)
+          else:
+              # Filter by the selected price range
+              queryset_list = queryset_list.filter(price__lte=int(price))
+
+  context = {
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'price_choices': price_choices,
+    'listings': queryset_list,
+    'values': request.GET
+  }
+
+  return render(request, 'listings/search.html', context)
